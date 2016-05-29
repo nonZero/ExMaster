9da2de2a37bd73352758a2f0d8c33a7e114e6aab
Udi Oron <udioron@gmail.com>
2016-05-29 12:05:25 +0300

    added totals

--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,16 @@
+from django.db.models import Sum
 from django.shortcuts import render
 
 from . import models
 
-def list_expenses(request):
 
+def list_expenses(request):
     qs = models.Expense.objects.all()
+
+    # total = sum(x.amount for x in qs)
+    total = qs.aggregate(sum=Sum('amount'))['sum']
 
     return render(request, 'expenses/expense_list.html', {
         'object_list': qs,
+        'total': total,
     })
