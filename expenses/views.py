2fe5de9cd1ff84e2f14158238d75aefa2a29efd7
Udi Oron <udioron@gmail.com>
2016-05-29 14:23:20 +0300

    cleanup

--- expenses/views.py
+++ expenses/views.py
@@ -1,10 +1,8 @@
 import datetime
+
 from django.db.models import Sum
-from django.shortcuts import render, redirect
-from django.views.generic import FormView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
-from django import forms
 
 from . import models
 
@@ -16,38 +14,6 @@ class ListExpensesView(ListView):
         return self.get_queryset().aggregate(sum=Sum('amount'))['sum']
 
 
-class ExpenseForm(forms.ModelForm):
-    accept_terms_of_service = forms.BooleanField()
-    class Meta:
-        model = models.Expense
-        exclude = (
-            'date',
-        )
-
-
-# def create_expense(request):
-#     if request.method == "POST":
-#         form = ExpenseForm(request.POST)
-#         if form.is_valid():
-#             form.instance.date = datetime.date.today()
-#             o = form.save()
-#             return redirect("/")
-#     else:  # GET
-#         form = ExpenseForm()
-#     return render(request, "expenses/expense_form.html", {
-#         'form': form,
-#     })
-
-# class CreateExpenseView(FormView):
-#     form_class = ExpenseForm
-#     template_name = "expenses/expense_form.html"
-#
-#     def form_valid(self, form):
-#         form.instance.date = datetime.date.today()
-#         o = form.save()
-#         return redirect("/")
-#
-
 class CreateExpenseView(CreateView):
     model = models.Expense
     # form_class = ExpenseForm
@@ -56,7 +22,7 @@ class CreateExpenseView(CreateView):
         'title',
     )
 
-    success_url = "/" # UGLY
+    success_url = "/"  # UGLY
 
     def form_valid(self, form):
         form.cleaned_data['accept_terms_of_service']
