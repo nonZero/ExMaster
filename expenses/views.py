475480c0538f553962ba3b9c552ccd950663fefa
Udi Oron <udioron@gmail.com>
2016-05-29 13:01:07 +0300

    added form handling - on success

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django import forms
 
@@ -13,9 +13,22 @@ class ListExpensesView(ListView):
         return self.get_queryset().aggregate(sum=Sum('amount'))['sum']
 
 
+class ExpenseForm(forms.Form):
+    date = forms.DateField()
+    amount = forms.DecimalField(max_digits=12, decimal_places=2)
+    title = forms.CharField(max_length=300)
 
 
 def create_expense(request):
     if request.method == "POST":
-        assert False, request.POST
-    return render(request, "expenses/expense_form.html")
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            o = models.Expense(**form.cleaned_data)
+            o.full_clean()
+            o.save()
+            return redirect("/")
+        assert False, ("NOT OK", form.errors)
+    form = ExpenseForm()
+    return render(request, "expenses/expense_form.html", {
+        'form': form,
+    })
